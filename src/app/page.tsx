cceba23862f77d2350f4cd252a26181d3a5ded08
christian <christian.verzosa@novare.com.hk>
2023-09-22 21:33:53 +0800

    feat: DnD components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 export default function Home() {
@@ -43,26 +44,73 @@ export default function Home() {
           {/* Board Page: Columns */}
           <div className="flex-1 grid grid-cols-3 gap-3 pt-3">
             {/* Board Page: Open Column */}
-            <div className="bg-gray-100 p-2">
-              {/* Task Card */}
-              <div className="bg-gray-200 p-2 mb-3">
-                <p className="text-[12px] mb-2">
-                  VOY-1865 [Welcome Treat] - (Unique Promo Code)
-                </p>
-                <div className="bg-white p-2">
-                  {/* Task Card: Title */}
-                  <p className="text-sm mb-4">QA Functional Test</p>
-                  {/* Task Card: Footer */}
-                  <div className="grid grid-cols-2">
-                    <div>-</div>
-                    <div className="flex flex-row items-center justify-end space-x-2">
-                      <p className="text-sm">VOY-1865</p>
-                      <div className="h-6 w-6 rounded-full bg-blue-500" />
-                    </div>
+            <DragDropContext
+              onDragUpdate={() => {}}
+              onDragEnd={() => console.log("drag end")}
+            >
+              <Droppable droppableId="droppable-1">
+                {(provided) => (
+                  <div
+                    className="bg-gray-100 p-2"
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >
+                    <Draggable draggableId="draggable-0" index={0}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="bg-gray-200 p-2 mb-3"
+                        >
+                          <p className="text-[12px] mb-2">
+                            VOY-1865 [Welcome Treat] - (Unique Promo Code) 1
+                          </p>
+                          <div className="bg-white p-2">
+                            {/* Task Card: Title */}
+                            <p className="text-sm mb-4">QA Functional Test</p>
+                            {/* Task Card: Footer */}
+                            <div className="grid grid-cols-2">
+                              <div>-</div>
+                              <div className="flex flex-row items-center justify-end space-x-2">
+                                <p className="text-sm">VOY-1865</p>
+                                <div className="h-6 w-6 rounded-full bg-blue-500" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                    <Draggable draggableId="draggable-1" index={1}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className="bg-gray-200 p-2 mb-3"
+                        >
+                          <p className="text-[12px] mb-2">
+                            VOY-1865 [Welcome Treat] - (Unique Promo Code) 2
+                          </p>
+                          <div className="bg-white p-2">
+                            {/* Task Card: Title */}
+                            <p className="text-sm mb-4">QA Functional Test</p>
+                            {/* Task Card: Footer */}
+                            <div className="grid grid-cols-2">
+                              <div>-</div>
+                              <div className="flex flex-row items-center justify-end space-x-2">
+                                <p className="text-sm">VOY-1865</p>
+                                <div className="h-6 w-6 rounded-full bg-blue-500" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
                   </div>
-                </div>
-              </div>
-            </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           </div>
         </div>
       </section>
